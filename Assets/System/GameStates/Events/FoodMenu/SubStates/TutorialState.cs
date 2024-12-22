3942d800bac5250f2abcdee8fba02190743c67c5
yasersyafa <syafayaser@gmail.com>
2024-12-22 22:20:10 +0700

    Add feature: Game States

---
+++ Assets/System/GameStates/Events/FoodMenu/SubStates/TutorialState.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Food_Menu
+{
+    public class TutorialState : IGameState
+    {
+        public void Enter(StateManager state)
+        {
+
+        }
+
+        public void Execute(StateManager state)
+        {
+
+        }
+
+        public void Exit(StateManager state)
+        {
+
+        }
+    }
+    
+}
