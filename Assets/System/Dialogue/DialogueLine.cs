9cdb188f44f4dc9cf682c09058dc7731c714b6ee
yasersyafa <syafayaser@gmail.com>
2025-02-09 01:29:33 +0700

    Delete: Folder Structure

--- Assets/System/Dialogue/DialogueLine.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class DialogueLine
-{
-    public DialogueCharacter dialogueCharacter;
-    [TextArea(3, 10)] public string line;
-}
--- Assets/System/Dialogue/Dialogues.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class Dialogues
-{
-    public List<DialogueLine> dialogueLines = new();
-}
